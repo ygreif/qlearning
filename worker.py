7f7ea5857e4c2ac9eb90e4fc0bb17ad227ef64f2
Yaron <ygreif@gmail.com>
2018-03-11 09:19:00 -0700

    Training example

--- worker.py
+++ worker.py
@@ -4,6 +4,21 @@ import gym
 from trainer import naftrainer
 from agents.agent import NoExploration
 
+import tensorflow as tf
+params = {'naf': {'learningParameters': {'discount': 0.99,
+                                         'learning_rate': 0.001,
+                                         'keep_prob': .9},
+                  'nnpParameters': {'hidden_layers': [1, 1],
+                                    'nonlinearity': tf.nn.tanh,
+                                    'use_dropout': False},
+                  'nnqParameters': {'hidden_layers': [400, 400],
+                                    'nonlinearity': tf.nn.tanh,
+                                    'use_dropout': True},
+                  'nnvParameters': {'hidden_layers': [800, 800],
+                                    'nonlinearity': tf.nn.tanh,
+                                    'use_dropout': True}},
+          'strat': {'decay': 800, 'scale': 1}}
+
 
 def worker(name, params, attempts, max_epochs, num):
     env = gym.make(name)
@@ -28,7 +43,7 @@ def helper(args):
 def runworkers(name, num_params, max_epochs, num_runs, max_workers):
     args = []
     for i in range(num_params):
-        params = naftrainer.random_parameters()
+#        params = naftrainer.random_parameters()
         args.append((name, params, num_runs, max_epochs, i))
     with ProcessPoolExecutor(max_workers=max_workers) as executor:
         r = [r for r in executor.map(helper, args)]
@@ -36,4 +51,5 @@ def runworkers(name, num_params, max_epochs, num_runs, max_workers):
 
 
 if __name__ == '__main__':
-    print runworkers('Pendulum-v0', 1, 1, 1, 1)
+    import timeit
+    print timeit.timeit("runworkers('Pendulum-v0', 1, 1, 1, 1)", setup="from __main__ import runworkers")
